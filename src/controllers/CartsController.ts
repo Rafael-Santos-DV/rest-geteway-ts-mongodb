d189188656ec939eab53089a7379d44783f61e43
Rafael Santos <santosrafaell187@gmail.com>
2022-04-19 17:20:28 -0300

    created crud with mongodb

--- src/controllers/CartsController.ts
+++ src/controllers/CartsController.ts
@@ -5,12 +5,12 @@ import '../database/index';
 class CartsController {
   async index(req: Request, res: Response) {
     try {
-      const result = Cart.find();
+      const result = await Cart.find();
       console.log(result);
-      return res.status(201).json();
+      return res.status(201).json(result);
     } catch (err) {
       console.log(err);
-      return res.status(500).json({ erro: 'erro' });
+      return res.status(500).json({ error: 'Internal server error.' });
     }
   }
 
@@ -21,7 +21,37 @@ class CartsController {
 
       return res.status(200).json(result);
     } catch (err) {
-      return res.status(500).json({ message: '' });
+      return res.status(500).json({ error: 'Internal server error.' });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { code, price } = req.body;
+      const cart = await Cart.findById(id);
+      if (!cart) return res.status(404).json();
+
+      await Cart.updateOne({ code, price });
+
+      return res.status(200).json();
+    } catch (err) {
+      return res.status(500).json({ error: 'Internal server error.' });
+    }
+  }
+
+  async destroy(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const cart = await Cart.findById(id);
+
+      if (!cart) return res.status(404).json();
+
+      await Cart.deleteOne();
+
+      return res.status(200).json();
+    } catch (err) {
+      return res.status(500).json({ error: 'Internal server error.' });
     }
   }
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,7 +3,9 @@ import CartsController from '../controllers/CartsController';
 
 const Routes = Router();
 
-Routes.get('/', CartsController.index);
-Routes.post('/createCart', CartsController.create);
+Routes.get('/carts', CartsController.index);
+Routes.post('/carts', CartsController.create);
+Routes.put('/carts/:id', CartsController.update);
+Routes.delete('/carts/:id', CartsController.destroy);
 
 export default Routes;
